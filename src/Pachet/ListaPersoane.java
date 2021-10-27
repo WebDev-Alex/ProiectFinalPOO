85f16a8d43ca30f1edb5e5e0256faf165a499b89
Alex <proalex999@yahoo.com>
2021-10-27 20:30:53 +0300

    Versiune de baza 0.1

--- src/Pachet/ListaPersoane.java
+++ src/Pachet/ListaPersoane.java
@@ -1,7 +1,7 @@
 package Pachet;
 
 import java.util.ArrayList;
-
+//Lista de persoane cred ca o putem face clasa de tip singleton
 public class ListaPersoane {
     //Lista cu obiecte tip presoana
     ArrayList<Persoana> persoane = new ArrayList<>();
